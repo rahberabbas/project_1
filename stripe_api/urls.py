b540fd326ea4162bb1eac333d407aae11454c703
Rahber Abbas Abidi <rahberabbasabidi@Rahbers-MacBook-Air.local>
2021-06-13 01:37:40 +0530

    first commit

---
+++ stripe_api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from stripe_api import views
+
+urlpatterns = [
+    path('test-payment/', views.test_payment),
+    path('save-stripe-info/', views.save_stripe_info),
+    path('confirm-payment-intent/', views.confirm_payment_intent)
+]
